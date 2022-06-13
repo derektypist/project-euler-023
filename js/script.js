289b653f2a26c25ff23f52ff9dd2d7a23aecadf7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-06-13 10:38:17 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -55,3 +55,9 @@ function sumOfNonAbundantNumbers(n) {
     }
     return sum;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
